6052fa8bc5d3d3579741fb21c092e0cbeda42b65
jalal <ramijalal1997@gmail.com>
2024-10-30 22:27:59 +0100

    made the featuredbox adapt for responsive screens

--- js/featuredbox/featuredbox.js
+++ js/featuredbox/featuredbox.js
@@ -11,20 +11,20 @@ let container;
 // });
 
 function initialize_featured_box(containerSelector, tag = DEFAULT_TAG, limit = DEFAULT_LIMIT) {
-    const $container = $(containerSelector);
-    load_featuredbox($container, tag, limit);
+     container = $(containerSelector);
+    load_featuredbox(container, tag, limit);
 }
 // Load featured box
 function load_featuredbox($container, tag, limit) {
-    container = $container;
-    fetch_featured_template()
-    .then(() => load_featured_products(tag, limit))
-    .catch(handleError);
+    
+    fetch_featured_template($container)
+        .then(() => load_featured_products(tag, limit))
+        .catch(handleError);
 }
 
 // Load products
 function load_featured_products(tag, limit) {
-    console.log('limit ' + limit); 
+    console.log('limit ' + limit);
     makeAjaxRequest(
         "includes/seller/get_products_featured.inc.php",
         "GET",
@@ -41,8 +41,8 @@ function load_featured_products_success_callback(data) {
     box_template.find('#title').text(data.title);
     box_template.find('#under-title').text(data.undertitle);
     render_featured_product_card(data.products);
-    set_slider();
-    
+ 
+
 }
 
 // Error callback for loading products
@@ -52,19 +52,21 @@ function load_featured_products_error_callback(xhr, status, error) {
 
 // Render product cards
 function render_featured_product_card(products) {
-    const $slider = $('.featured-box-slider').empty();
+    const $slider =  container.find('.featured-box-slider').empty();
+    
     products.forEach(product => {
         const $card = create_featured_product_card(product);
         $slider.append($card);
-      //  $('.featured-box-slider').slick('setPosition');
-    
+        //  $('.featured-box-slider').slick('setPosition');
+
     });
+    set_slider();
 }
 
 // Create product card element
 function create_featured_product_card(product) {
     const $card = featured_product_template.clone();
-    
+
     $card.find(".featured-card-title").text(product.title);
     $card.find(".price-featured").text(product.price + " درهم");
     const images = product.images.length > 0 ? JSON.parse(product.images) : [product.image];
@@ -74,43 +76,51 @@ function create_featured_product_card(product) {
 
 // Set up slider
 function set_slider() {
-  //  const $slider = $('.featured-box-slider');
+    //  const $slider = $('.featured-box-slider');
+    const $slider = container.find('.featured-box-slider');
+    if ($slider.hasClass('slick-initialized')) {
+        $slider.slick('unslick');
+    }
+    $slider.slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        infinite: true,
+        cssEase: 'ease-in-out',
+        autoplay: true,
+        autoplaySpeed: 10000,
+        prevArrow: '<div class="slider-prev"></div>',
+        nextArrow: '<div class="slider-next"></div>',
+        responsive: [
+            {
+                breakpoint: 1024, // When the screen is less than 1024px
+                settings: {
+                    slidesToShow: 4, // Show 3 slides
+                    slidesToScroll: 1,
+                    dots: true // Show dots
+                }
+            },
+            {
+                breakpoint: 768, // When the screen is less than 768px
+                settings: {
+                    slidesToShow: 2, // Show 2 slides
+                    slidesToScroll: 1,
 
-  $('.featured-box-slider').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: true,
-    infinite: true,
-    cssEase: 'ease-in-out',
-    autoplay: true,
-    autoplaySpeed: 1000,
-    prevArrow: '<div class="slider-prev"></div>',
-    nextArrow: '<div class="slider-next"></div>',
-    responsive: [
-        {
-            breakpoint: 1200,  // Large tablets and smaller desktops
-            settings: { slidesToShow: 4 }
-        },
-        {
-            breakpoint: 992,   // Tablets and larger phones
-            settings: { slidesToShow: 2 }
-        },
-        {
-            breakpoint: 768,   // Small tablets and smaller phones
-            settings: { slidesToShow: 1 }
-        }
-    ]
-});
+                }
+            },
 
-    
+        ]
+    });
+
+
 }
 
 // Fetch template
-function fetch_featured_template() {
+function fetch_featured_template($container) {
     return $.get("layouts/featuredbox.layout.html").then(data => {
         console.log("Fetched Template:", data);
         box_template = $(data);
-        $('#featuredbox_container').append(box_template);
+        $container.append(box_template);
         featured_product_template = box_template.find('#featured-product-template').clone().removeAttr("id");
     }).fail((jqXHR, textStatus, errorThrown) => {
         handleError("Error fetching the featured box template:", textStatus, errorThrown);
@@ -125,7 +135,7 @@ function handleError(message, status = '', error = '') {
 }
 
 
-$(window).on('load', function() {
-   
-   // set_slider();
-  });
+$(window).on('load', function () {
+
+    // set_slider();
+});
